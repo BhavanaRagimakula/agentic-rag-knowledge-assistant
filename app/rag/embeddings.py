e670de6e016d572bca1542f2aa411016b246a20c
BhavanaRagimakula <bhavana84myself@gmail.com>
2026-08-31 13:18:39 +0530

    Update and rename embeddings.py to app/rag/embeddings.py

---
+++ app/rag/embeddings.py
@@ -0,0 +1,16 @@
+agentic-rag-knowledge-assistant/
+│
+├── app/
+│   ├── __init__.py
+│   └── rag/
+│       ├── __init__.py
+│       ├── ingestion.py
+│       ├── vector_store.py
+│       └── embeddings.py
+│
+├── data/
+│   └── documents/
+│       └── company_policy.txt
+│
+├── .gitignore
+└── requirements.txt
